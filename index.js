6e0ef4ec1ff35963e23e9b0dea16a793f257359d
Lídor1103 <lidor.piero@gmail.com>
2023-09-01 18:04:59 -0500

    go

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const navVisible = document.querySelector(".a");
 
 buttonMenu.addEventListener("click", () => {
     navUl.classList.toggle("nav-menu-visible");
-    navVisible.classList.toggle("nav-despliegue"); 
+    navVisible.classList.toggle("nav-despliegue");
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     const buttonMenu = document.querySelector(".conteiner-menu-icon");
-    
+
     buttonMenu.addEventListener("click", () => {
         navUl.classList.toggle("nav-menu-visible");
     });
@@ -28,19 +28,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const nav = document.querySelector(".a");
 
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 0) {
-                nav.style.backgroundColor = "#132d46";
-            } else {
-                nav.style.backgroundColor = "transparent";
-            }
-        });
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        nav.style.backgroundColor = "#132d46";
+    } else {
+        nav.style.backgroundColor = "transparent";
+    }
+});
 
 
 
 const btnMode = document.querySelector(".mode");
+const logoImage = document.querySelector(".logo img");
 
-btnMode.addEventListener("click" , () => {
+btnMode.addEventListener("click", () => {
     document.body.classList.toggle("modonoche");
     btnMode.classList.toggle("nocheactivo");
-})
+
+    // Verificar si el modo noche está activado o desactivado y cambiar el src en consecuencia
+    if (document.body.classList.contains("modonoche")) {
+        logoImage.src = "img/logo.png";
+    } else {
+        logoImage.src = "img/logo-negativo.png";
+    }
+});
